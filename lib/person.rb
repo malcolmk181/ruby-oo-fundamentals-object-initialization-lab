72db460a1abb8c1bfba1f492f8e0a5b4c48a4389
Malcolm Keyes <3.malcolmk@gmail.com>
2022-05-30 13:57:30 -0400

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,15 @@
+class Person
+    # peeps!
+
+    def initialize(name)
+        @name = name
+    end
+
+    def talk
+        puts "Hello World!"
+    end
+
+    def walk
+        puts "The Person is walking"
+    end
+end
